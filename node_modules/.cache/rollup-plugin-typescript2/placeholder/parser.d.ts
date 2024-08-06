b7a371a88c5baf5763fa994a9c31e31e05bd880b
Vincent <vincent.castel@worldline.com>
2024-08-06 16:37:45 +0200

    add node modules

---
+++ node_modules/.cache/rollup-plugin-typescript2/placeholder/parser.d.ts
@@ -0,0 +1,3 @@
+import { ResponseType } from "./types/responseType";
+export declare const parse: (response: any, responseType?: ResponseType | undefined) => Promise<any>;
+export declare const parseResponse: (response: any, responseType?: ResponseType | undefined) => Promise<any>;
